edfbc28d4ca40ec08976c2490cea900ea1ee91d6
alapatipranavi <alapatipranavi@gmail.com>
2026-02-21 19:23:11 +0530

    Final combined deployment server setup

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const expertRoutes = require("./routes/expertRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
@@ -20,18 +21,17 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+/* ------------------ DATABASE CONNECTION ------------------ */
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
+/* ------------------ API ROUTES ------------------ */
 app.use("/experts", expertRoutes);
 app.use("/bookings", bookingRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Server Running");
-});
-
-/* SOCKET CONNECTION */
+/* ------------------ SOCKET CONNECTION ------------------ */
 io.on("connection", (socket) => {
   console.log("User connected");
 
@@ -43,6 +43,16 @@ io.on("connection", (socket) => {
     console.log("User disconnected");
   });
 });
+
+/* ------------------ SERVE FRONTEND (PRODUCTION) ------------------ */
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
+/* ------------------ SERVER START ------------------ */
 
 const PORT = process.env.PORT || 5000;
 
